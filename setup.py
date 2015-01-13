f48d0632e4af7108e282388830ec67637a1f396f
Peter Bingham <p.bingham@camlintechnologies.com>
2015-01-13 08:51:13 +0000

    added __init__

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dae_RelayBoard',
+      version='1.2',
+      description='Denkovi Relay Board Controller',
+      author='Peter Bingham',
+      author_email='petersbingham@hotmail.co.uk',
+      url='https://code.google.com/p/dae-py-relay-controller/',
+      packages=['dae_RelayBoard']
+     )
